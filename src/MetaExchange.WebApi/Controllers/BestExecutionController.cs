38114aa707bd727a42655fcb9f8b445aa0a757cb
stephane.mouako <stephane.mouako@sioux.eu>
2025-07-07 16:32:22 +0200

    feat: Add ExchangeController and BestExecutionRequest model for execution planning

---
+++ src/MetaExchange.WebApi/Controllers/BestExecutionController.cs
@@ -0,0 +1,31 @@
+﻿using MetaExchange.Application.DTOs;
+using MetaExchange.Application.Interfaces;
+using MetaExchange.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetaExchange.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExchangeController(IMetaExchangeService service, IExchangeRepository repository) : ControllerBase
+{
+    #region Public methods declaration
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ExecutionPlan), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetBestExecution([FromBody] BestExecutionRequest request)
+    {
+        var exchanges = await repository.GetAllAsync();
+        var executionPlan =
+            service.GetBestExecutionPlan(exchanges, request.OrderType, request.Amount);
+
+        if (!executionPlan.Orders.Any())
+            return NotFound("No execution orders could be generated for the given request.");
+
+        return Ok(executionPlan);
+    }
+
+    #endregion
+}
---
+++ src/MetaExchange.WebApi/Models/BestExecutionRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MetaExchange.Domain.Entities;
+
+namespace MetaExchange.WebApi.Models;
+
+public record BestExecutionRequest(
+    [Range(0.0001, double.MaxValue, ErrorMessage = "Amount must be greater than 0.")]
+    decimal Amount,
+    OrderType OrderType);
