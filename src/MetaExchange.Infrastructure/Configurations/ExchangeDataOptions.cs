67389d0b235739f1fa9c282ec745da716fb19461
stephane.mouako <stephane.mouako@sioux.eu>
2025-07-07 15:07:57 +0200

    Add DTOs and mappers for exchange and order management
    - Created AvailableFundsDto to represent available funds in crypto and Euro.
    - Developed ExchangeDto to encapsulate exchange details including available funds and order book.
    - Implemented OrderBookDto to manage lists of bids and asks.
    - Added OrderDto to represent individual orders with properties like ID, time, type, amount, and price.
    - Introduced OrderEntryDto to wrap OrderDto for order entries in the order book.
    - Built ExchangeMapper to convert ExchangeDto to domain Exchange entity.
    - Established JsonExchangeRepository to handle JSON file operations for loading exchanges.

---
+++ src/MetaExchange.Infrastructure/Configurations/ExchangeDataOptions.cs
@@ -0,0 +1,8 @@
+namespace MetaExchange.Infrastructure.Configuration;
+
+public class ExchangeDataOptions
+{
+    public const string c_SectionName = "ExchangeData";
+
+    public string DirectoryPath { get; set; } = string.Empty;
+}
---
+++ src/MetaExchange.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using MetaExchange.Application.Interfaces;
+using MetaExchange.Infrastructure.Configuration;
+using MetaExchange.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MetaExchange.Infrastructure;
+
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        ExchangeDataOptions exchangeDataOptions = new();
+        configuration.Bind(ExchangeDataOptions.c_SectionName, exchangeDataOptions);
+
+        services.AddScoped<IExchangeRepository>(_ =>
+            new JsonExchangeRepository(exchangeDataOptions.DirectoryPath));
+        return services;
+    }
+
+}
